a9a8d692d91e5f35e3a013eaf4b3623fc4b2852c
Christopher Frew <chris@mawson.tech>
2024-03-14 23:29:34 +0930

    groups defined in host not as child groups...

--- 1-configure-services/ansible-groups.tf
+++ 1-configure-services/ansible-groups.tf
@@ -1,6 +1,5 @@
 resource "ansible_group" "insecure_hosts" {
   name = "insecure_hosts"
-  children = ["okd-dns-0", "okd-dns-1"]
   variables = {
     ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
   }
