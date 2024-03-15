6469b9b62216dfda5f9c3bed596471dbfcbff807
Christopher Frew <chris@mawson.tech>
2024-03-15 22:51:35 +0930

    setup loadbalancer

--- 1-configure-services/ansible-playbooks.tf
+++ 1-configure-services/ansible-playbooks.tf
@@ -17,16 +17,31 @@ resource "ansible_playbook" "okd-dns-1" {
   }
 }
 
+resource "ansible_playbook" "okd-loadbalancer-0" {
+  playbook = "playbooks/okd-loadbalancer-0.yml"
+  name = ansible_host.okd-loadbalancer-0.name
+  replayable = true
+  extra_vars = {
+    ansible_ssh_extra_args = "-oStrictHostKeyChecking=no"
+  }
+}
+
+resource "ansible_playbook" "okd-loadbalancer-1" {
+  playbook = "playbooks/okd-loadbalancer-1.yml"
+  name = ansible_host.okd-loadbalancer-1.name
+  replayable = true
+  extra_vars = {
+    ansible_ssh_extra_args = "-oStrictHostKeyChecking=no"
+  }
+}
+
 //resource "ansible_playbook" "okd-dhcp-0" {
 //  playbook = "playbooks/okd-dhcp-0.yml"
 //  name = "ansible_host.okd-dhcp-0.name"
 //  replayable = true
 //}
-//resource "ansible_playbook" "okd-loadbalancer-0" {
-//  playbook = "playbooks/okd-loadbalancer-0.yml"
-//  name = "ansible_host.okd-loadbalancer-0.name"
-//  replayable = true
-//}
+
+
 //
 //resource "ansible_playbook" "okd-nfs-0" {
 //  playbook = "playbooks/okd-nfs-0.yml"
